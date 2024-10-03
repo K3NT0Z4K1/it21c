8a7b35cd8cae13001fd843249d5d7534c4dd6dd9
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-10-03 13:17:26 +0800

    Added Object Person

---
+++ L01 - Objects and Classes/topic1.js
@@ -0,0 +1,13 @@
+
+//input
+const person1 ={
+    name:"Kent Ryan L. Pagongpong",
+    age:20,
+    occupation:"Student"
+}
+
+//Process and output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+
