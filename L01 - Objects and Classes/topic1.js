0c14e7157f80cd2114285171164217abaddc8248
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-10-03 13:20:10 +0800

    Added Classes

--- L01 - Objects and Classes/topic1.js
+++ L01 - Objects and Classes/topic1.js
@@ -11,3 +11,13 @@ console.log("Name: "+person1.name);
 console.log("Age: "+person1.age);
 console.log("Occupation: "+person1.occupation);
 
+
+//classes
+class Person {
+    constructor(name, age, occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+}
+
